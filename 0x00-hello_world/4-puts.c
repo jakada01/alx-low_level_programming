407c8ec80877c61ef55c926cee3e7dbbda7f80be
Yusuf_Jakada <yusufjakada5@gmail.com>
2022-09-09 03:43:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main-entry point 
+ *
+ * return 0 after printing the function.
+ */
+int main(void)
+{ 
+       	puts("programming is like building multilingual puzzle");
+
+        return (0);
+}
