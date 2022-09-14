1038eeab0751a976fe99af78dd1c3d19a8a31ec9
Yusuf_Jakada <yusufjakada5@gmail.com>
2022-09-13 21:57:58 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function to check if c is a letter, lowercase or uppercase
+ * @c: is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _abs(int c)
+{
+if (c > 0 || c==0)
+{
+return (c);
+}
+else
+return (c * -1);
+}
